b30364c5b921a48878786fc37028b7e157de5bfe
Bhavesh Sompura <56756309+Bhavesh-Sompura@users.noreply.github.com>
2024-06-15 22:46:55 +0530

    data update
    Noodemon added
    api added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is Running");
@@ -11,4 +13,12 @@ app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
-app.listen(5000, console.log("Server Started on Port 5000"));
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  req.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log("Server Started on PORT  ${PORT}"));

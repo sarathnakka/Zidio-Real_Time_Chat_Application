cf90d7884a92e3e790322350002766dfb881465f
Bhavesh Sompura <56756309+Bhavesh-Sompura@users.noreply.github.com>
2024-06-15 16:02:21 +0530

    API Testing Data Added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
 app.listen(5000, console.log("Server Started on Port 5000"));

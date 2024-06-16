c47cbb90a4e6425d75b3fdd6ada17e3341eaeb5e
Sarath Nakka <101663778+sarathnakka@users.noreply.github.com>
2024-06-16 14:27:02 +0530

    UI updated

--- frontend/src/components/MyCharts.jsx
+++ frontend/src/components/MyCharts.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { useToast } from "@chakra-ui/toast";
+import { Box, Stack, Text } from "@chakra-ui/layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const MyCharts = () => {
+import { ChatState } from "../Context/ChatProvider";
+
+const MyChats = ({ fetchAgain }) => {
+  const [loggedUser, setLoggedUser] = useState();
+  const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
+  const toast = useToast();
+  const fetchChats = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get("/api/chat", config);
+      setChats(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the chats",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
+  useEffect(() => {
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
+    fetchChats();
+  }, [fetchAgain]);
+
   return (
-    <div>
-      my charts
-    </div>
-  )
-}
+    <Box
+      d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+    </Box>
+  );
+};
 
-export default MyCharts
+export default MyChats;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import ChatProvider from "./Context/ChatProvider.jsx/index.js";
+import ChatProvider from "./Context/ChatProvider.jsx";
+
 
 
 

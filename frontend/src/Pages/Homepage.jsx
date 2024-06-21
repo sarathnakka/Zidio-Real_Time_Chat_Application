816de1121081ba7abd5973b2c622116be700e252
Saloni-Soni03 <143264887+Saloni-Soni03@users.noreply.github.com>
2024-06-21 22:32:15 +0530

    Update

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,47 +1,55 @@
-import React from "react";
-import {Container, Box, Text, Tab, TabList, TabPanel,TabPanels, Tabs,} from "@chakra-ui/react";
-import Login from "../components/Authentication/Login";
-import Signup from "../components/Authentication/Signup";
+import React from 'react';
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup';
+import { Container, Box, Text ,Tabs, TabList, TabPanels, Tab, TabPanel} from '@chakra-ui/react';
+
 
 
 const Homepage = () => {
+  return (
+    <div className="App" style={{ position: 'absolute', top: '20px', right: '100px' }}> 
+      <Container maxW="xl" centerContent>
+        <Box
+          display='flex'
+          justifyContent='center'
+          p={3}
+          bg="white"
+          margin="40px 0 15px 0"
+          width="500px"  // Adjust the width directly here
+          borderRadius="10px" 
+          borderWidth="1px"
+          backgroundColor="#153448"
+          
+        >
 
-  const history = useHistory();
-
-  useEffect(() => {
-      const user = JSON.parse(localStorage.getItem("user"));
-      setUser(user);
-      if (!user) {
-          history.push("/charts");
-      }
-
-  },[history]);
-
-  return <Container maxW="xl" centerContent>
-  <Box
-  d="flex"
-  justifyContent="center"
-  p={3}
-  bg="white"
-  w="100%"
-  m="40px 0 15px 0"
-  borderRadius="lg"
-  borderWidth="1px">
-  <Text fontSize="4xl" fontFamily="Work sans">Zidio Chat Application  </Text>
-  </Box>
-  <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-  <Tabs variant="soft-rounded">
-          <TabList mb="1em">
-            <Tab width="50%">Login</Tab>
-            <Tab width="50%">Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel> <Login /> </TabPanel>
-            <TabPanel> <Signup /> </TabPanel>
-          </TabPanels>
-        </Tabs>
-  </Box>
-  </Container>;
+          <Text fontSize="4xl" fontFamily="Work sans" color={'white'}>Talkify</Text>
+        </Box>
+        <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="1px"
+        backgroundColor='#DFF5FF'
+      >
+    <Tabs variant='soft-rounded' colorScheme='blue'>
+  <TabList mb='1em'>
+    <Tab width="50%" >Login</Tab>
+    <Tab width="50%" >Sign Up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+   <Login/>
+    </TabPanel>
+    <TabPanel>
+    <Signup/>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+      </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default Homepage;

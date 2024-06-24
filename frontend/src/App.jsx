dbe06f08859a1832a05a1d96207959ac809ad54c
Bhavesh Sompura <56756309+Bhavesh-Sompura@users.noreply.github.com>
2024-06-24 16:36:59 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
+import { Route } from "react-router-dom";
 import Chatpage from "./Pages/Chatpage";
 
 function App() {
